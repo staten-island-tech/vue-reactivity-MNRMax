6b842779ec824d85c0552a5618f5d32058b363ee
MNRMax <maximilian.shabatayev@gmail.com>
2023-03-09 12:38:41 -0500

    sdfghjkl;'

---
+++ vite-project/src/views/BuildView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from 'vue'
+import SelectItem from '../components/SelectItem.vue'
+const childMsg = ref(0)
+</script>
+
+<template>
+  <main>
+    <SelectItem @response="(msg) => childMsg = msg" />
+    <p>{{ childMsg }}</p>
+  </main>
+</template>
